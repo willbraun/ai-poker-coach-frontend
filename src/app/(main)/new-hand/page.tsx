93440104fdef89493f57aaaa7180fb05aead47d1
willbraun <williamhbraun1@gmail.com>
2024-04-29 16:10:28 -0600

    added first field with react hook form and zod

--- src/app/(main)/new-hand/page.tsx
+++ src/app/(main)/new-hand/page.tsx
@@ -6,53 +6,120 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import TypographyH1 from '@/components/ui/typography/TypographyH1'
+
+const zodCardGroup = z.object({
+	player: z.number(),
+	cards: z.array(
+		z.object({
+			value: z.string(),
+			suit: z.string(),
+		})
+	),
+	evaluation: z.string(),
+})
+
+const zodAction = z.object({
+	player: z.number(),
+	decision: z.number(),
+	bet: z.number(),
+})
+
+const FormSchema = z.object({
+	name: z.string(),
+	gameStyle: z.number(),
+	playerCount: z.number(),
+	position: z.number(),
+	smallBlind: z.number(),
+	bigBlind: z.number(),
+	ante: z.number(),
+	bigBlindAnte: z.number(),
+	myStack: z.number(),
+	notes: z.string(),
+	round0Cards: zodCardGroup,
+	round0Actions: z.array(zodAction),
+	round1Cards: zodCardGroup,
+	round1Actions: z.array(zodAction),
+	round2Cards: zodCardGroup,
+	round2Actions: z.array(zodAction),
+	round3Cards: zodCardGroup,
+	round3Actions: z.array(zodAction),
+	villains: z.array(zodCardGroup),
+})
 
 const NewHand = () => {
-	const form = useForm()
-
-	const [formValue, setFormValue] = useState<NewHandState>({
-		name: '',
-		gameStyle: 0,
-		playerCount: 0,
-		position: 0,
-		smallBlind: 0,
-		bigBlind: 0,
-		ante: 0,
-		bigBlindAnte: 0,
-		myStack: 0,
-		notes: '',
-		round0Cards: {} as FormCardGroup,
-		round0Actions: [],
-		round1Cards: {} as FormCardGroup,
-		round1Actions: [],
-		round2Cards: {} as FormCardGroup,
-		round2Actions: [],
-		round3Cards: {} as FormCardGroup,
-		round3Actions: [],
-		villains: [],
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+		defaultValues: {
+			name: '',
+			gameStyle: 0,
+			playerCount: 0,
+			position: 0,
+			smallBlind: 0,
+			bigBlind: 0,
+			ante: 0,
+			bigBlindAnte: 0,
+			myStack: 0,
+			notes: '',
+			round0Cards: {
+				player: 0,
+				cards: [],
+				evaluation: '',
+			},
+			round0Actions: [],
+			round1Cards: {
+				player: 0,
+				cards: [],
+				evaluation: '',
+			},
+			round1Actions: [],
+			round2Cards: {
+				player: 0,
+				cards: [],
+				evaluation: '',
+			},
+			round2Actions: [],
+			round3Cards: {
+				player: 0,
+				cards: [],
+				evaluation: '',
+			},
+			round3Actions: [],
+			villains: [],
+		},
 	})
+
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = form
 
 	return (
 		<main className='mt-24'>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-					<FormField
-						control={form.control}
-						name='username'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Username</FormLabel>
-								<FormControl>
-									<Input placeholder='shadcn' {...field} />
-								</FormControl>
-								<FormDescription>This is your public display name.</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type='submit'>Submit</Button>
-				</form>
-			</Form>
+			<div className='max-w-screen-sm mx-auto'>
+				<TypographyH1 className='mb-8'>Add New Hand</TypographyH1>
+				<Form {...form}>
+					<form onSubmit={handleSubmit(data => console.log(data))} className='space-y-8'>
+						<FormField
+							control={control}
+							name='name'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Title</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type='submit'>Submit</Button>
+					</form>
+				</Form>
+			</div>
 		</main>
 	)
 }
