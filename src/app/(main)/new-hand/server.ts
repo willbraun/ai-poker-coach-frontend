da3ed8e8331d9f92ceaadc64e4305c4940a2f0aa
willbraun <williamhbraun1@gmail.com>
2024-06-05 08:41:46 -0400

    added getPotActions function

--- src/app/(main)/new-hand/server.ts
+++ src/app/(main)/new-hand/server.ts
@@ -20,18 +20,91 @@ export const analyze = async (prevState: any, formData: FormData) => {
 		}
 	}
 
-	const { name, gameStyle, playerCount, position, smallBlind, bigBlind, ante, bigBlindAnte, myStack, notes } =
-		parsed.data
-	console.log(parsed.data)
+	const {
+		name,
+		gameStyle,
+		playerCount,
+		position,
+		smallBlind,
+		bigBlind,
+		ante,
+		bigBlindAnte,
+		myStack,
+		notes,
+		rounds,
+		villains,
+	} = parsed.data
 
-	// create handsteps object
+	const pots = [
+		{
+			potIndex: 0,
+			winner: '',
+		},
+	]
+
+	let currentStep = 0
+	let currentPotIndex = 0
+
+	const getPotActions = (round: FormRound) => {
+		const actions = round.actions
+
+		const playerBets: { [player: number]: number } = {}
+		actions
+			.filter(action => action.decision !== 'fold')
+			.forEach(action => {
+				const { player, bet } = action
+				if (playerBets[player] === undefined) {
+					playerBets[player] = 0
+				}
+				playerBets[player] += bet
+			})
+
+		// if all aggregate bets are the same, one pot
+		const entries = Object.entries(playerBets)
+		if (entries.every(([_, bet]) => bet === entries[0][1])) {
+			return entries.map(
+				([player, bet]) =>
+					({
+						potIndex: currentPotIndex,
+						player: parseInt(player),
+						bet,
+					} as PotAction)
+			)
+		}
+
+		// if not, create potActions for side pots
+		const sortedEntries = entries.toSorted((a, b) => a[1] - b[1])
+		let entryProgress = [...sortedEntries]
+		let result: PotAction[] = []
+
+		while (entryProgress.length > 0) {
+			const currentLowestBet = entryProgress[0][1]
+			result.push(
+				...entryProgress.map(
+					([player, _]) =>
+						({
+							step: currentStep,
+							potIndex: currentPotIndex,
+							player: parseInt(player),
+							bet: currentLowestBet,
+						} as PotAction)
+				)
+			)
+			entryProgress = entryProgress
+				.map(([player, bet]) => [player, bet - currentLowestBet] as [string, number])
+				.filter(([_, bet]) => bet > 0)
+			currentPotIndex++
+		}
+
+		return result
+	}
 
 	// create steps
 	// iterate over formData.rounds and create Round objects
-	// const round0 = parsed
 
 	// create cards from card group
 	// create actions from action group
+	// create potActions from round actions
 
 	// create villains
 	// iterate over formData.villains and create Villain objects
@@ -74,6 +147,14 @@ export const analyze = async (prevState: any, formData: FormData) => {
 
 	// const data = await res.json()
 
+	// check for analysis response type
+
+	// if (!isAuthData(data)) {
+	// 	return {
+	// 		error: `Unknown error: ${JSON.stringify(data)}`,
+	// 	}
+	// }
+
 	// const analysis = data.analysis
 	const analysis = 'analysis'
 
@@ -85,14 +166,6 @@ export const analyze = async (prevState: any, formData: FormData) => {
 		analysis: analysis,
 		error: '',
 	}
-
-	// check for analysis response type
-
-	// if (!isAuthData(data)) {
-	// 	return {
-	// 		error: `Unknown error: ${JSON.stringify(data)}`,
-	// 	}
-	// }
 }
 
 export const postHand = async (handSteps: HandSteps, analysis: string) => {
