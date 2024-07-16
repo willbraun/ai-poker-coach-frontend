21760656970b1c224f7f5d6a25c00f1513494e36
willbraun <williamhbraun1@gmail.com>
2024-07-16 08:05:48 -0400

    removed overscrolling on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,41 +15,43 @@ const HomeCard: FC<{ message: string }> = ({ message }) => {
 
 const Home: FC = () => {
 	return (
-		<main className='h-fit min-h-full w-full overflow-y-auto bg-gradient-to-br from-primary to-gray-900'>
-			<div className='flex flex-wrap items-center justify-evenly gap-x-8 gap-y-16 px-8 py-16'>
-				<div className='flex flex-col items-center gap-4 text-center'>
-					<h1 className='inline-block text-6xl font-bold tracking-tight text-white sm:text-8xl'>AI Poker Coach</h1>
-					<p className='text-3xl text-white'>Poker advice in English, not charts</p>
+		<main className='fixed h-full w-full bg-gradient-to-br from-primary to-gray-900'>
+			<div className='h-full overflow-y-auto'>
+				<div className='flex flex-wrap items-center justify-evenly gap-x-8 gap-y-16 px-8 py-16'>
+					<div className='flex flex-col items-center gap-4 text-center'>
+						<h1 className='inline-block text-6xl font-bold tracking-tight text-white sm:text-8xl'>AI Poker Coach</h1>
+						<p className='text-3xl text-white'>Poker advice in English, not charts</p>
+					</div>
+					<Image src={robot} alt='poker robot' width={400} className='rounded-3xl shadow-lg' />
 				</div>
-				<Image src={robot} alt='poker robot' width={400} className='rounded-3xl shadow-lg' />
-			</div>
-			<div className='grid grid-cols-1 gap-8 p-8 md:grid-cols-3'>
-				<HomeCard message={'Review hand analysis in English, rather than deciphering complex charts'} />
-				<HomeCard message={'Record hands to your account, and learn from other users'} />
-				<HomeCard message={'Powered by GPT-3.5, enhanced with poker training data'} />
-			</div>
-			<div className='mt-16 flex w-full justify-center px-8 pb-16'>
-				<div className='flex w-full max-w-screen-md flex-col gap-4'>
-					<Button className='h-fit w-full p-4'>
-						<Link href='/create-account' className='text-3xl'>
-							Sign up
-						</Link>
-					</Button>
-					<div className='flex w-full gap-4'>
-						<Button className='h-fit w-full p-4' variant='secondary'>
-							<Link href='/login' className='text-3xl'>
-								Log in
+				<div className='grid grid-cols-1 gap-8 p-8 md:grid-cols-3'>
+					<HomeCard message={'Review hand analysis in English, rather than deciphering complex charts'} />
+					<HomeCard message={'Record hands to your account, and learn from other users'} />
+					<HomeCard message={'Powered by GPT-3.5, enhanced with poker training data'} />
+				</div>
+				<div className='mt-16 flex w-full justify-center px-8 pb-16'>
+					<div className='flex w-full max-w-screen-md flex-col gap-4'>
+						<Button className='h-fit w-full p-4'>
+							<Link href='/create-account' className='text-3xl'>
+								Sign up
 							</Link>
 						</Button>
-						<Button className='h-fit w-full p-4' variant='secondary'>
-							<Link href='/feed' className='text-3xl'>
-								Open app
-							</Link>
-						</Button>
+						<div className='flex w-full gap-4'>
+							<Button className='h-fit w-full p-4' variant='secondary'>
+								<Link href='/login' className='text-3xl'>
+									Log in
+								</Link>
+							</Button>
+							<Button className='h-fit w-full p-4' variant='secondary'>
+								<Link href='/feed' className='text-3xl'>
+									Open app
+								</Link>
+							</Button>
+						</div>
 					</div>
 				</div>
+				<Footer textColor='text-white' iconFill='fill-white' />
 			</div>
-			<Footer textColor='text-white' iconFill='fill-white' />
 		</main>
 	)
 }
