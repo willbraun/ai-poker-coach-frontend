e8ac77969d92514a7844209982d0b53304471187
willbraun <williamhbraun1@gmail.com>
2024-08-26 19:55:14 -0400

    refactored create account page, changed unnecessary state and effect to derived state

--- src/app/(auth)/create-account/page.tsx
+++ src/app/(auth)/create-account/page.tsx
@@ -5,76 +5,23 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { createAccount } from './server'
 import Link from 'next/link'
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import FormError from '@/components/FormError'
 import PasswordField from '@/components/PasswordField'
 import { Home } from 'lucide-react'
 
-interface PasswordValidation {
-	sixChars: boolean
-	lowercase: boolean
-	uppercase: boolean
-	digit: boolean
-	nonAlphanumeric: boolean
-}
-
-const CreateAccountFormDetails: FC = () => {
+const CreateAccount: FC = () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
-	const [passwordValidation, setPasswordValidation] = useState<PasswordValidation>({
-		sixChars: false,
-		lowercase: false,
-		uppercase: false,
-		digit: false,
-		nonAlphanumeric: false,
-	})
-
+	const sixChars = password.length >= 6
+	const lowercase = /[a-z]/.test(password)
+	const uppercase = /[A-Z]/.test(password)
+	const digit = /[0-9]/.test(password)
+	const nonAlphanumeric = /\W/.test(password)
 	const { pending } = useFormStatus()
 
-	const disabled = !email || !password || Object.values(passwordValidation).includes(false) || pending
+	const disabled = !email || !password || pending || !sixChars || !lowercase || !uppercase || !digit || !nonAlphanumeric
 
-	useEffect(() => {
-		setPasswordValidation({
-			sixChars: password.length >= 6,
-			lowercase: /[a-z]/.test(password),
-			uppercase: /[A-Z]/.test(password),
-			digit: /[0-9]/.test(password),
-			nonAlphanumeric: /\W/.test(password),
-		})
-	}, [password])
-
-	return (
-		<>
-			<div className='mb-4'>
-				<label htmlFor='email'>Email</label>
-				<Input
-					type='email'
-					name='email'
-					value={email}
-					onChange={e => {
-						setEmail(e.target.value)
-					}}
-				/>
-			</div>
-			<PasswordField password={password} setPassword={setPassword} className={'mb-4'} />
-			<div className='mb-8'>
-				<p>Password requirements</p>
-				<ul>
-					<li>{passwordValidation.sixChars ? '✅' : '❌'} At least six characters</li>
-					<li>{passwordValidation.lowercase ? '✅' : '❌'} At least one lowercase letter (a-z)</li>
-					<li>{passwordValidation.uppercase ? '✅' : '❌'} At least one uppercase letter (A-Z)</li>
-					<li>{passwordValidation.digit ? '✅' : '❌'} At least one digit (0-9) </li>
-					<li>{passwordValidation.nonAlphanumeric ? '✅' : '❌'} At least one non-alphanumeric character </li>
-				</ul>
-			</div>
-			<Button type='submit' disabled={disabled} className='mb-4 w-full text-lg'>
-				{pending ? 'Creating Account...' : 'Create Account'}
-			</Button>
-		</>
-	)
-}
-
-const CreateAccount: FC = () => {
 	const initialState = {
 		error: '',
 	}
@@ -87,7 +34,31 @@ const CreateAccount: FC = () => {
 				<div className='w-96 p-4'>
 					<h1 className='mb-4 text-3xl font-bold'>Create Account</h1>
 					<form action={formAction}>
-						<CreateAccountFormDetails />
+						<div className='mb-4'>
+							<label htmlFor='email'>Email</label>
+							<Input
+								type='email'
+								name='email'
+								value={email}
+								onChange={e => {
+									setEmail(e.target.value)
+								}}
+							/>
+						</div>
+						<PasswordField password={password} setPassword={setPassword} className={'mb-4'} />
+						<div className='mb-8'>
+							<p>Password requirements</p>
+							<ul>
+								<li>{sixChars ? '✅' : '❌'} At least six characters</li>
+								<li>{lowercase ? '✅' : '❌'} At least one lowercase letter (a-z)</li>
+								<li>{uppercase ? '✅' : '❌'} At least one uppercase letter (A-Z)</li>
+								<li>{digit ? '✅' : '❌'} At least one digit (0-9) </li>
+								<li>{nonAlphanumeric ? '✅' : '❌'} At least one non-alphanumeric character </li>
+							</ul>
+						</div>
+						<Button type='submit' disabled={disabled} className='mb-4 w-full text-lg'>
+							{pending ? 'Creating Account...' : 'Create Account'}
+						</Button>
 					</form>
 					<FormError error={state.error} />
 					<p className='mb-2 mt-8'>
