4fbc0962a6f8d772cdfa1f766ed1423ef6cb6458
willbraun <williamhbraun1@gmail.com>
2024-09-25 14:12:09 -0400

    added google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,33 @@
-import type { Metadata } from 'next'
+import { FC, ReactNode } from 'react'
+import { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
 	title: 'AI Poker Coach',
-	description: 'Created by Will Braun',
+	description: 'Poker advice in English, not charts',
 }
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode
-}>) {
+const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
 	return (
 		<html lang='en'>
-			<body id='body'>{children}</body>
+			<body id='body'>
+				{children}
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+					strategy='afterInteractive'
+				/>
+				<Script id='google-analytics' strategy='afterInteractive'>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+					`}
+				</Script>
+			</body>
 		</html>
 	)
 }
+
+export default RootLayout
